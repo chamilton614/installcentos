86182d7226959b7fe21ce8e5ebc3f3b269de0b0c
Chad J. Hamilton <chad.j.hamilton@gmail.com>
2019-04-28 11:32:05 -0400

    Updated logic to fix uninstall

--- uninstall_openshift.sh
+++ uninstall_openshift.sh
@@ -4,12 +4,20 @@ echo #########################################################
 echo #              Uninstall OpenShift v3.11                #
 echo #########################################################
 
-[ ! -d logs] && mkdir logs
+CURRENT=$(pwd)
 
-[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
+if [ ! -d logs ]; then
+  mkdir logs
+fi
+
+if [ ! -d openshift-ansible ]; then
+  git clone https://github.com/openshift/openshift-ansible.git
+fi
+
 cd openshift-ansible && git fetch && git checkout release-3.11 && cd ..
 
 #Run the uninstallation
-ansible-playbook playbooks/uninstall_openshift.yaml | tee logs/openshift_uninstall.log 2>&1
+echo ansible-playbook -i $CURRENT/inventory.ini $CURRENT/playbooks/uninstall_openshift.yaml | tee logs/openshift_uninstall.log 2>&1
+ansible-playbook -i $CURRENT/inventory.ini $CURRENT/playbooks/uninstall_openshift.yaml | tee logs/openshift_uninstall.log 2>&1
 
 echo Uninstall has been completed.
