21194917049989666ab9a9142862dbac5283b973
chamilton <chad.hamilton@prolifics.com>
2019-06-24 01:34:50 -0400

    Updated for Single OKD

--- install_openshift.sh
+++ install_openshift.sh
@@ -143,7 +143,7 @@ cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
 #${IP}		$(hostname) console console.${DOMAIN}
-#${IP}		$(hostname) console.${DOMAIN} console.apps.${DOMAIN}
+${IP}		$(hostname) console.${DOMAIN} console.apps.${DOMAIN}
 EOD
 
 if [ -z $DISK ]; then 
